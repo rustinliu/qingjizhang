c23ebef2eb55c047cffe1a2fdb77e3ca38677bf7
rustin <1543153730@qq.com>
2022-01-05 23:28:30 +0800

    完善

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -7,6 +7,9 @@
             :value.sync="interval"
             height="48px"
         /> -->
+        <div class="chart-wrapper" ref="chartWrapper">
+            <Chart :options="chartOptions" class="chart" />
+        </div>
         <ol v-if="groupList.length > 0">
             <li v-for="(group, index) in groupList" :key="index">
                 <h3 class="title">
@@ -35,14 +38,16 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import Tab from "@/components/Tabs.vue";
-import intervalList from "@/constans/intervalList";
 import typeList from "@/constans/typeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
+import Chart from "@/components/Chart.vue";
+import _ from "lodash";
 
 @Component({
     components: {
         Tab,
+        Chart,
     },
 })
 export default class Statistics extends Vue {
@@ -91,7 +96,6 @@ export default class Statistics extends Vue {
             const last = result[result.length - 1];
             if (dayjs(last.title).isSame(dayjs(current.creatdAt), "day")) {
                 last.items.push(current);
-                console.log(result);
             } else {
                 result.push({
                     title: dayjs(current.creatdAt).format("YYYY-MM-DD"),
@@ -112,8 +116,67 @@ export default class Statistics extends Vue {
     }
     //interval = "day";
     // intervalList = intervalList;
-    type = "+";
+    type = "-";
     typeList = typeList;
+    get keyvalueList() {
+        let today = new Date();
+        const array = [];
+        for (let i = 0; i <= 29; i++) {
+            const date = dayjs(today).subtract(i, "day").format("YYYY-MM-DD");
+            const found = _.find(this.groupList, {
+                title: date,
+            });
+            array.push({
+                date: date,
+                value: found ? found.total : 0,
+            });
+        }
+        array.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return array;
+    }
+    get chartOptions() {
+            grid: {
+                left: 0,
+                right: 0,
+            },
+            xAxis: {
+                type: "category",
+                axisTick: { alignWithLabel: true },
+                data: keys,
+                axisLine: {
+                    lineStyle: { coolr: "#666" },
+                },
+                axisLabel: {
+                    formatter: function (value: string, index: number) {
+                        return value.substr(5);
+                    },
+                },
+            },
+            yAxis: {
+                type: "value",
+                show: false,
+            },
+            series: [
+                {
+                    itemStyle: { borderWidth: 1, color: "#666" },
+                    symbolSize: 20,
+                    symbol: "circle",
+                    data: values,
+                    type: "line",
+                },
+            ],
+            tooltip: {
+                show: true,
+                triggerOn: "click",
+                formatter: "{c}",
+                position: "top",
+            },
+        };
+    }
+    mounted() {
+        const div = this.$refs.chartWrapper as HTMLDivElement;
+        div.scrollLeft = div.scrollWidth;
+    }
 }
 </script>
 
@@ -159,5 +222,14 @@ export default class Statistics extends Vue {
 .noResult {
     padding: 16px;
     text-align: center;
+}
+.chart {
+    width: 430%;
+    &-wrapper {
+        overflow: auto;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
 }
 </style>
