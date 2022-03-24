8d4a3312e90df6f507479c4638d3cb837f1f08ac
rustin <1543153730@qq.com>
2022-03-24 18:10:42 +0800

    favico图标问题解决

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Plugin = require('svg-sprite-loader/plugin');
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/accountbook-website/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/qingjizhang-website/' : '/',
     lintOnSave: false,
     chainWebpack: (config) => {
         const dir = path.resolve(__dirname, 'src/assets/icons');
@@ -18,5 +18,14 @@ module.exports = {
             .end();
         config.plugin('svg-sprite').use(Plugin, [{ plainSprite: true }]);
         config.module.rule('svg').exclude.add(dir); // 其他 svg loader 排除 icons目录
+    },
+    pwa: {
+        iconPaths: {
+            favicon32: './public/favicon.png',
+            favicon16: './public/favicon.png',
+            appleTouchIcon: './public/favicon.png',
+            maskIcon: './public/favicon.png',
+            msTileImage: './public/favicon.png',
+        },
     },
 };
